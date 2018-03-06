d5f3e7ceaf4d60528b61e5cf961635fc416e0a62
Camilo Yate <cyateya35439@universidadean.edu.co>
2018-03-06 13:47:47 -0500

    Update ejercicio8.py
    completando el ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,3 +1,18 @@
+ """
+    (num1,float,num3) -> str
+    Escribe el mensaje 'Cuando Pasen", años, " Años con un ", interes, "de interes usted habra ganado", resultado, "Euros"
+    >>> intereses_euros(5000.8.5,10)
+    'Cuando Pasen 10  Años con un  8.5 de interes usted habra ganado 11305 Euros'
+    
+    >>> intereses_euros(2000.4.5,5)
+    'Cuando Pasen 5  Años con un  4.5 de interes usted habra ganado 2492 Euros'
+   
+    :param num, floatante: el numero de euros, intereses, tiempo
+    :return: el mensaje resultante
+    """
+
+
+
 def principal():
     euros = int(input("Cuantos Euros"))
     interes = float(input("Cuanto interes"))
